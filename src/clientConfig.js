4af76e9b6b67e8d8f12034148b7c99fe78d60b9b
sajumulakkal <sajumulakkal@hotmail.com>
2026-08-01 14:17:02 +0800

    Update socketURI to new production server

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -2,7 +2,8 @@ const config = {
   isProduction: process.env.NODE_ENV === 'production',
   contentfulSpaceId: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
   contentfulAccessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
-  socketURI: process.env.REACT_APP_SERVER_URI || 'https://sparkling-gecko-148372.netlify.app/', 
+  //socketURI: process.env.REACT_APP_SERVER_URI || 'https://sparkling-gecko-148372.netlify.app/', 
+  socketURI: process.env.REACT_APP_SERVER_URI || 'https://pokerserver-production-b6bc.up.railway.app/', 
 };
 
 export default config;
